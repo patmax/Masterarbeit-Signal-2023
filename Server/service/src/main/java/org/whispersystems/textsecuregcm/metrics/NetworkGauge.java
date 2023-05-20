389b351f5f03f6ed77dda27399012de925b8da00
Patrick Ahrens <patrick.ahrens@rub.de>
2023-05-20 19:40:47 +0200

    Correction: CWE-459 & CWE-772

--- Server/service/src/main/java/org/whispersystems/textsecuregcm/metrics/NetworkGauge.java
+++ Server/service/src/main/java/org/whispersystems/textsecuregcm/metrics/NetworkGauge.java
@@ -18,24 +18,25 @@ public abstract class NetworkGauge implements Gauge<Double> {
 
   protected Pair<Long, Long> getSentReceived() throws IOException {
     File           proc          = new File("/proc/net/dev");
-    BufferedReader reader        = new BufferedReader(new FileReader(proc));
-    String         header        = reader.readLine();
-    String         header2       = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader(proc))) {
+      reader.readLine();
+      reader.readLine();
 
-    long           bytesSent     = 0;
-    long           bytesReceived = 0;
+      long           bytesSent     = 0;
+      long           bytesReceived = 0;
 
-    String interfaceStats;
+      String interfaceStats;
 
-      while ((interfaceStats = reader.readLine()) != null) {
-        String[] stats = interfaceStats.split("\\s+");
+        while ((interfaceStats = reader.readLine()) != null) {
+          String[] stats = interfaceStats.split("\\s+");
 
-        if (!stats[1].equals("lo:")) {
-          bytesReceived += Long.parseLong(stats[2]);
-          bytesSent     += Long.parseLong(stats[10]);
+          if (!stats[1].equals("lo:")) {
+            bytesReceived += Long.parseLong(stats[2]);
+            bytesSent     += Long.parseLong(stats[10]);
+          }
         }
-      }
 
-    return new Pair<>(bytesSent, bytesReceived);
+      return new Pair<>(bytesSent, bytesReceived);
+    }
   }
 }
